b6f56512563a6be34112f6a6738e85b741f8c7e9
Fatou TOURE <fatou.toure@epita.fr>
2020-05-12 11:24:54 +0200

    trosieme commit

--- midterm-2/Cryptanalysis/Program.cs
+++ midterm-2/Cryptanalysis/Program.cs
@@ -8,7 +8,7 @@ internal static class Program
     {
         // Put your tests here.
         
-        Console.WriteLine(Tools.Histogram("Aa c"));
+        Console.WriteLine(Tools.RotChar('l',25));
         Test();
     }
 
--- midterm-2/Cryptanalysis/Vigenere.cs
+++ midterm-2/Cryptanalysis/Vigenere.cs
@@ -5,19 +5,65 @@ namespace Cryptanalysis
 public class Vigenere
 {
     public const float FrenchIndexOfCoincidence = 0.0778F;
+
+    private string key;
     public Vigenere(string key)
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < key.Length; i++)
+        {
+            if (Tools.LetterIndex(key[i]) == -1)
+            {
+                throw new ArgumentException("the key must only contain characters");
+            }
+            
+        }
+
+        this.key = key;
     }
 
     public string Encrypt(string msg)
     {
-        throw new NotImplementedException();
+        int indexKey = 0;
+        int indexList = key.Length;
+        string res = "";
+
+        foreach (var c in msg)
+        {
+            if (Tools.LetterIndex(c) != -1)
+            {
+                res += Tools.RotChar(c,Tools.LetterIndex(key[indexKey]));
+                indexKey = Tools.Modulus(indexKey + 1, indexList);
+            }
+            else
+            {
+                res += c;
+            }
+        }
+
+        return res;
     }
 
     public string Decrypt(string cypherText)
     {
-        throw new NotImplementedException();
+        
+        int indexKey = 0;
+        int indexList = key.Length;
+        string res = "";
+
+        foreach (var c in cypherText)
+        {
+            if (Tools.LetterIndex(c) != -1)
+            {
+                res += Tools.RotChar(c,- Tools.LetterIndex(key[indexKey]));
+                indexKey = Tools.Modulus(indexKey + 1, indexList);
+            }
+            else
+            {
+                res += c;
+            }
+        }
+
+        return res;
     }
 
     public static string GuessKeyWithLength(string cypherText, int keyLength)
