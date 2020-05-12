e73d6fcf49382619764312b3cc4a6b95e5cf52cf
Fatou TOURE <fatou.toure@epita.fr>
2020-05-12 10:46:40 +0200

    premier commit

---
+++ midterm-2/Cryptanalysis/Tools.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+
+namespace Cryptanalysis
+{
+public static class Tools
+{
+    public static int Modulus(int a, int b)
+    {
+        if (b < 0)
+            b = -b;
+        
+        var mod = a % b;
+        return mod < 0 ? b + mod : mod;
+    }
+
+    public static int LetterIndex(char c)
+    {
+        int ord = Convert.ToInt32(c);
+
+        if (ord < 65 || ord > 122)
+        {
+            return -1;
+        }
+        else
+        {
+            if (ord <= 90)
+            {
+                return ord - 65;
+            }
+            if (ord >= 97)
+            {
+                return ord - 97;
+            }
+            
+            return -1;
+        }
+    }
+
+    public static char RotChar(char c, int n)
+    {
+        char letter = c;
+        int nOrd = 0;
+        int nAscii = 0;
+
+        if (c < 65 || c > 122)
+        {
+            return c;
+        }
+        else
+        {
+
+            if (c >= 65 && c <= 90)
+            {
+                nOrd = LetterIndex(c);
+                nAscii = Modulus(nOrd + n, 26);
+
+                letter = Convert.ToChar(nAscii + 65);
+            }
+
+            if (c >= 97 && c <= 122)
+            {
+                nOrd = LetterIndex(c);
+                nAscii = Modulus(nOrd + n, 26);
+
+                letter = Convert.ToChar(nAscii + 97);
+            }
+
+            return letter;
+        }
+    }
+
+    public static int[] Histogram(string str)
+    {
+        int[] index = new int[26];
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (LetterIndex(str[i]) != -1)
+            {
+                index[LetterIndex(str[i])]++;
+            }
+        }
+
+        return index;
+    }
+    
+    public static string FilterLetters(string str)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static string Extract(string str, int start, int step)
+    {
+        throw new NotImplementedException();
+    }
+}
+}
